453f5b60e0b4f2449b1d405ecb83b41c675945eb
Ninaj <72307870+Ninaji@users.noreply.github.com>
2023-05-15 16:06:25 -0300

    Add files via upload
    Projeto Finalizado

---
+++ src/js/index.js
@@ -0,0 +1,21 @@
+const botaoAlterarTema = document.getElementById("botao-alterar-tema");
+
+const body = document.querySelector("body");
+
+const imagemBotaoTrocaDeTema = document.querySelector(".imagem-botao")
+
+botaoAlterarTema.addEventListener("click", () => {
+    const modoEscutoEstaAtivo = body.classList.contains("modo-escuro")
+    if(modoEscutoEstaAtivo){
+        body.classList.remove("modo-escuro");
+        imagemBotaoTrocaDeTema.setAttribute("src", "/src/images/sun.png");
+    }
+    else{
+        body.classList.add("modo-escuro");
+        imagemBotaoTrocaDeTema.setAttribute("src", "/src/images/moon.png");
+    }
+    
+    
+    
+    
+})
